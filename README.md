bcca8f5158b36bc1489a32063f5b21d860f31d7c
gauri <gauri.kate@datamato.com>
2024-01-09 15:08:15 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<a href="https://lockurl.netlify.app/"><img src="https://bit.ly/3ZRfJEy"></a>
+
+# 🔒 LockURL - Password Protect Links
+[![Netlify Status](https://api.netlify.com/api/v1/badges/72e6b5dc-96f8-4c8c-af94-a9c626cd2db1/deploy-status)](https://app.netlify.com/sites/lockurl/deploys)‎ ‎
+[![Buy Me A Coffee](https://img.shields.io/open-vsx/stars/redhat/java?color=D8B024&label=buy%20me%20a%20coffee&style=flat)](https://www.buymeacoffee.com/utsanjan)‎ ‎
+[![](https://dcbadge.vercel.app/api/server/uavTPkr?style=flat)](https://discord.gg/ZuuWJm7MR3)‎ ‎ 
+[![](https://img.shields.io/github/license/utsanjan/LockURL?logoColor=red&style=flat)](https://github.com/utsanjan/LockURL/blob/main/LICENSE)‎ ‎
+[![](https://img.shields.io/github/languages/count/utsanjan/LockURL?style=flat)](https://github.com/utsanjan/LockURL/search?l=shell)‎ ‎
+[![](https://img.shields.io/github/languages/top/utsanjan/LockURL?color=light%20green&style=flat)](https://github.com/utsanjan/LockURL)‎ ‎
+<br>
+
+LockURL is a utility for encrypting and decrypting URLs. An encrypted URL will prompt the user for a password when they visit it. LockURL gets the original URL and routes users there if the password is valid. If not, an error message is shown. Additionally, users can add hints to show next to the password prompt. Each encrypted URL is contained within the link that the program creates. Users, therefore, have complete control over the data they generate via LockURL. Nothing is ever kept on the server.
+#### [✅ Click here to see working example of a LockURL link.](https://lockurl.netlify.app/#eyJ2IjoiMC4wLjEiLCJlIjoiSGZlcHBodFJvZjgrYm55ZlR6Qkx4RlJWMEdvdXUxRWxIUHFweStNaWg2RTVKU2F1YjQweDhRPT0iLCJoIjoiMSsxPT8g8J+YgyIsInMiOiIrSHNnd3FsSUZHMVk0c2lrcmY2TjVRPT0iLCJpIjoiU2h5RzNUdjJsdmljaWNKRyJ9)
+
+## 🛠️ Usage Guide
+- Create a password protected link from [here](https://lockurl.netlify.app/).
+- Use the advanced options when creating a link to customise the encryption method.
+- By default, the initialization vector and salt are randomized for security, but this can be disabled, even though doing so is a vulnerability.
+- If you lose the password, it is almost impossible to recover the original link. The strong security guaranteed by encryption can be a blessing or a curse if you are not careful!
+- Currently, the only way to recover a lost password is by trying all possible options very slowly by brute force. An example application to brute force LockURL links in the browser can be found [here](https://lockurl.netlify.app/bruteforce/).
+- If you receive a LockURL link that you don't trust, decrypt it using the option given in the homepage.
+
+## 📞 Contacts
+For Queries: [My Instagram Profile](https://www.instagram.com/utsanjan/)  
+[Check Out My YouTube Channel](https://www.youtube.com/DopeSatan)
---
+++ home/js/front.js
@@ -0,0 +1,76 @@
+"use strict";
+
+function resizeForm(){
+    var width=(window.innerWidth>0)?window.innerWidth:document.documentElement.clientWidth;
+    if (width > 1024) {document.getElementById("social-menu").checked = true;
+    document.getElementById("social-menu").style.visibility = "hidden";
+    document.getElementById("ham1").style.visibility = "hidden";
+    document.getElementById("ham2").style.visibility = "hidden";
+    document.getElementById("ham3").style.visibility = "hidden"} 
+    else {document.getElementById("social-menu").checked = false;
+    document.getElementById("social-menu").style.visibility = "visible";
+    document.getElementById("ham1").style.visibility = "visible";
+    document.getElementById("ham2").style.visibility = "visible";
+    document.getElementById("ham3").style.visibility = "visible"}
+    if (window.self !== window.top)
+    {document.getElementById("page").style.visibility = "visible"}
+    else {window.location.href="https://lockurl.netlify.app";}
+    }window.onresize = resizeForm;
+    resizeForm();
+    
+document.addEventListener("DOMContentLoaded", function () {
+    /* =====================================
+		TESTIMONIALS SLIDER
+	======================================== */
+    var testimonialsSlider = new Swiper(".testimonials-slider", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+            clickable: true,
+        },
+    });
+
+    /* =====================================
+		VIDEO MODAL
+	======================================== */
+    new ModalVideo(".js-modal-btn");
+
+    /* =====================================
+		NAVBAR BEHAVIOR
+	======================================== */
+    window.addEventListener("scroll", navBehavior);
+    window.addEventListener("load", navBehavior);
+
+    function navBehavior() {
+        if (window.pageYOffset > 5) {
+            document.querySelector(".navbar").classList.add("active");
+        } else {
+            document.querySelector(".navbar").classList.remove("active");
+        }
+
+        if (window.pageYOffset > 1000) {
+            document.querySelector("#scrollTop").classList.add("active");
+        } else {
+            document.querySelector("#scrollTop").classList.remove("active");
+        }
+    }
+
+    /* =====================================
+		MOVE TO TOP OF THE PAGE
+	======================================== */
+    document
+        .getElementById("scrollTop")
+        .addEventListener("click", function (e) {
+            e.preventDefault();
+            window.scrollTo(0, 0);
+        });
+
+    /* =====================================================
+		BOOTSTRAP SCROLLSPY
+	===================================================== */
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: "#navbar",
+    });
+});
